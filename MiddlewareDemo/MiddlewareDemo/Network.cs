91db3d3bf5824e3df506a5c66e6cc96afef2e9c1
Saddaf Afrin <Saddaf Afrin@DESKTOP-GFRC3H1>
2020-12-09 23:42:03 +0100

    (Middleware) - Implemented a simple demo middleware

---
+++ MiddlewareDemo/MiddlewareDemo/Network.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiddlewareDemo
+{
+    static class Network
+    {
+        static readonly public List<IServer> servers = new List<IServer>();
+    }
+}
